95a1feb60bf15cd252608c76d50cb7f248598d21
dang9805 <congdang981997@gmail.com>
2025-10-28 23:56:21 +0700

    add ...

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: './',
   server: {
     proxy: {
       // Mọi yêu cầu FE gửi đến '/api' sẽ được chuyển tiếp
